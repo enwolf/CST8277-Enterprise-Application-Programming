044bb879766d235580c7575c042cdd05cd6243e4
enwolf <35503014+enwolf@users.noreply.github.com>
2024-09-14 02:29:51 -0400

    Updated Java Class File: PhysicianManufacturer.java
    Added JavaDocs Comments.

--- Lab1-JDBC-CMD-Physician-Skeleton/src/main/java/jdbccmd/PhysicianManufacturer.java
+++ Lab1-JDBC-CMD-Physician-Skeleton/src/main/java/jdbccmd/PhysicianManufacturer.java
@@ -2,6 +2,22 @@
  * File:  PhysicianManufacturer.java Course Materials CST 8277
  * 
  * @author Robin Phillis
+ * @version 1.0
+ * @since 2024-09-14
+ * 
+ * @description This class extends `StringTypeManufacturerImpl` from the PODAM library to provide 
+ *              custom random data generation for fields in the `Physician` class. It uses predefined 
+ *              pools of data (e.g., last names, first names, specialties) and generates random email 
+ *              addresses and phone numbers. It initializes these data pools from text files and uses a 
+ *              `SecureRandom` instance for generating random values.
+ * 
+ * @see uk.co.jemos.podam.typeManufacturers.StringTypeManufacturerImpl
+ * @see uk.co.jemos.podam.api.AttributeMetadata
+ * @see uk.co.jemos.podam.api.DataProviderStrategy
+ * @see java.security.SecureRandom
+ * @see java.util.List
+ * @see java.util.Map
+ * @see java.util.Scanner
  */
 package jdbccmd;
 
@@ -23,103 +39,117 @@ import uk.co.jemos.podam.api.DataProviderStrategy;
 import uk.co.jemos.podam.typeManufacturers.StringTypeManufacturerImpl;
 
 /**
- * This class used the PODAM (POJO Data Mocker) library.  The idea is to provide the needed data and this class randomize
- * data for the given object.
+ * Custom implementation of `StringTypeManufacturerImpl` to generate random data for fields in the 
+ * `Physician` class. This implementation is specifically used to provide random values for attributes
+ * such as last names, first names, email addresses, phone numbers, and specialties.
+ * <p>
+ * The class uses predefined pools of data loaded from text files and a `SecureRandom` instance for 
+ * generating random values. The email addresses and phone numbers are generated according to specific 
+ * formats, while other attributes are selected from the loaded data pools.
+ * </p>
  * 
- * @author Robin Phillis
+ * @see uk.co.jemos.podam.typeManufacturers.StringTypeManufacturerImpl
+ * @see uk.co.jemos.podam.api.AttributeMetadata
+ * @see uk.co.jemos.podam.api.DataProviderStrategy
+ * @see java.security.SecureRandom
+ * @see java.util.List
+ * @see java.util.Map
+ * @see java.util.Scanner
  */
 public class PhysicianManufacturer extends StringTypeManufacturerImpl {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	//Name of the fields in the Physician class
+	/** Field name for last names in the `Physician` class */
 	private static final String LASTNAME_FIELD = "lastName";
+	/** Field name for first names in the `Physician` class */
 	private static final String FIRSTNAME_FIELD = "firstName";
+	/** Field name for email addresses in the `Physician` class */
 	private static final String EMAIL_FIELD = "email";
+	/** Field name for phone numbers in the `Physician` class */
 	private static final String PHONENUMBER_FIELD = "phoneNumber";
+	/** Field name for specialties in the `Physician` class */
 	private static final String SPECIALTY_FIELD = "specialty";
 
-	
-	//Name of the files with data in them
+	/** File containing pool of last names */
 	protected static final String POOL_OF_LASTNAMES = "lastnamePool.txt";
+	/** File containing pool of first names */
 	protected static final String POOL_OF_FIRSTNAMES = "firstnamePool.txt";
+	/** File containing pool of specialties */
 	protected static final String POOL_OF_SPECIALTIES = "specialtyPool.txt";
 	
-	//List of useful digits and letters
+	/** Characters used for generating random strings */
 	protected static final String ALPHA_LETTERS = "abcdefghijklmnopqrstuvwxyz";
 	protected static final String DIGITS = "1234567890";
 	
-	//Random generator which is less predictable than Random class
-	//https://stackoverflow.com/a/11052736/764951
+	/** Secure random number generator for better randomness */
 	protected static SecureRandom rnd = new SecureRandom();
 	
-	//List of loaded data pool
+	/** List of last names loaded from file */
 	protected static List<String> poolOfLastnames = new ArrayList<>();
+	/** List of first names loaded from file */
 	protected static List<String> poolOfFirstnames = new ArrayList<>();
+	/** List of specialties loaded from file */
 	protected static List<String> poolOfSpecialties = new ArrayList<>();
 
-	//Load the pool of data into the list
-	static 
-	{
-		
+	/** Static block to initialize data pools from files */
+	static {
 		ClassLoader theClassLoader = MethodHandles.lookup().lookupClass().getClassLoader();
 		
 		try (
-			
 			InputStream firstnamePoolInputStream = theClassLoader.getResourceAsStream(POOL_OF_FIRSTNAMES);
 			InputStream lastnamePoolInputStream = theClassLoader.getResourceAsStream(POOL_OF_LASTNAMES);
 			InputStream specialtyPoolInputStream = theClassLoader.getResourceAsStream(POOL_OF_SPECIALTIES);
-			
 			Scanner lastnamePoolScanner = new Scanner(lastnamePoolInputStream);
 			Scanner firstnamePoolScanner = new Scanner(firstnamePoolInputStream);
 			Scanner specialtyPoolScanner = new Scanner(specialtyPoolInputStream);
-			
-		    ) 
-		{
-			while (lastnamePoolScanner.hasNext()) 
-			{
+		) {
+			while (lastnamePoolScanner.hasNext()) {
 				poolOfLastnames.add(lastnamePoolScanner.nextLine());
 			}
 
-			while (firstnamePoolScanner.hasNext()) 
-			{
+			while (firstnamePoolScanner.hasNext()) {
 				poolOfFirstnames.add(firstnamePoolScanner.nextLine());
 			}
 			
-			while (specialtyPoolScanner.hasNext()) 
-			{
+			while (specialtyPoolScanner.hasNext()) {
 				poolOfSpecialties.add(specialtyPoolScanner.nextLine());
 			}
 	
-		} 
-		catch (IOException e) 
-		{
-			logger.error("something went wrong building pools:  {}", e.getLocalizedMessage());
+		} catch (IOException e) {
+			logger.error("Error loading data pools: {}", e.getLocalizedMessage());
 		}
-
 	}
 
-	//Depending on the attribute name, create random data for that field and return it.
+	/**
+	 * Generates a random value for a given attribute based on its name.
+	 * <p>
+	 * Depending on the attribute name, this method generates random data for fields such as email, 
+	 * last name, first name, phone number, or specialty. If the attribute name does not match any 
+	 * predefined field, it falls back to the default implementation in `StringTypeManufacturerImpl`.
+	 * </p>
+	 * 
+	 * @param strategy The data provider strategy used to generate the data.
+	 * @param attributeMetadata Metadata about the attribute for which data is being generated.
+	 * @param genericTypesArgumentsMap A map of generic type arguments.
+	 * @return A randomly generated string suitable for the given attribute.
+	 */
 	@Override
 	public String getType(DataProviderStrategy strategy, AttributeMetadata attributeMetadata,
-			Map< String, Type> genericTypesArgumentsMap) 
-	{
+			Map<String, Type> genericTypesArgumentsMap) {
 		
 		String stringType = "";
 		
-		if (EMAIL_FIELD.equals(attributeMetadata.getAttributeName())) 
-		{
-			//Create random email
+		if (EMAIL_FIELD.equals(attributeMetadata.getAttributeName())) {
+			// Generate a random email address
 			StringBuilder sb = new StringBuilder();
 			
-			while (sb.length() < 3) 
-			{
+			while (sb.length() < 3) {
 				int index = (int) (rnd.nextFloat() * ALPHA_LETTERS.length());
 				sb.append(ALPHA_LETTERS.charAt(index));
 			}
 			
-			while (sb.length() < 8) 
-			{
+			while (sb.length() < 8) {
 				int index = (int) (rnd.nextFloat() * DIGITS.length());
 				sb.append(DIGITS.charAt(index));
 			}
@@ -127,37 +157,21 @@ public class PhysicianManufacturer extends StringTypeManufacturerImpl {
 			sb.append("@algonquinlive.com");
 			stringType = sb.toString();
 			
-		} 
-		
-		else if (LASTNAME_FIELD.equals(attributeMetadata.getAttributeName())) 
-		{
+		} else if (LASTNAME_FIELD.equals(attributeMetadata.getAttributeName())) {
 			stringType = poolOfLastnames.get(rnd.nextInt(poolOfLastnames.size()));
-		} 
-		
-		else if (FIRSTNAME_FIELD.equals(attributeMetadata.getAttributeName())) 
-		{
+		} else if (FIRSTNAME_FIELD.equals(attributeMetadata.getAttributeName())) {
 			stringType = poolOfFirstnames.get(rnd.nextInt(poolOfFirstnames.size()));			
-		} 
-		
-		else if (PHONENUMBER_FIELD.equals(attributeMetadata.getAttributeName())) 
-		{
+		} else if (PHONENUMBER_FIELD.equals(attributeMetadata.getAttributeName())) {
 			int npa = rnd.nextInt(643) + 100;
 			int extension = rnd.nextInt(9000) + 1000;
 			stringType = String.format("613%03d%04d", npa, extension);
-		} 
-		
-		else if (SPECIALTY_FIELD.equals(attributeMetadata.getAttributeName())) 
-		{
+		} else if (SPECIALTY_FIELD.equals(attributeMetadata.getAttributeName())) {
 			stringType = poolOfSpecialties.get(rnd.nextInt(poolOfSpecialties.size()));			
-		} 
-	
-		else 
-		{			
+		} else {			
 			stringType = super.getType(strategy, attributeMetadata, genericTypesArgumentsMap);			
 		}
 		
 		return stringType;
-		
 	}
 
 }

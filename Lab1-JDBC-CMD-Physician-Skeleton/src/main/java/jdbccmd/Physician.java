3d18815c4cf77281231659dfb07489fcc1f3ac67
enwolf <35503014+enwolf@users.noreply.github.com>
2024-09-13 22:31:07 -0400

    Updated Java Class File: Physician.java
    Added Protected String: specialty
    
    Added setter and getter for this data member.
    
    Updated toString() to add specialty value to final string.\
    
    Fixed Class Formating.

--- Lab1-JDBC-CMD-Physician-Skeleton/src/main/java/jdbccmd/Physician.java
+++ Lab1-JDBC-CMD-Physician-Skeleton/src/main/java/jdbccmd/Physician.java
@@ -1,7 +1,7 @@
 /*********************************************************************************************************
  * File:  Physician.java Course Materials CST8277
  * 
- * @author Teddy Yap
+ * @author Robin Phillis
  */
 package jdbccmd;
 
@@ -11,9 +11,10 @@ import java.time.LocalDateTime;
 /**
  * Simple POJO for physician.
  * 
- * @author Teddy Yap
+ * @author Robin Phillis
  */
 public class Physician implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 
 	protected int id;
@@ -21,97 +22,125 @@ public class Physician implements Serializable {
 	protected String firstName;
 	protected String email;
 	protected String phoneNumber;
-	//TODO Add the specialty field
+	protected String specialty;
 	protected LocalDateTime created;
 
-	public int getId() {
+	public int getId() 
+	{
 		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getFirstName() {
+	
+	public String getFirstName() 
+	{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public String getLastName() 
+	{
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) 
+	{
 		this.firstName = firstName;
 	}
 
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 	}
+	
+	public String getPhoneNumber() 
+	{
+		return phoneNumber;
+	}
+	
+	public String getSpecialty() 
+	{
+		return specialty;
+	}
+	
+	public LocalDateTime getCreated() 
+	{
+		return created;
+	}
+	
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
 
-	public void setEmail(String email) {
+	public void setLastName(String lastName) 
+	{
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) 
+	{
 		this.email = email;
 	}
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(String phoneNumber) 
+	{
 		this.phoneNumber = phoneNumber;
 	}
 
-	//TODO Add getter and setter methods for the specialty field
-
-	public LocalDateTime getCreated() {
-		return created;
+	public void setSpecialty(String specialty) 
+	{
+		this.specialty = specialty;
 	}
 
-	public void setCreated(LocalDateTime created) {
+	public void setCreated(LocalDateTime created) 
+	{
 		this.created = created;
 	}
 
 	@Override
-	public int hashCode() {
+	public int hashCode() 
+	{
 		return 1;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
+	public boolean equals(Object obj) 
+	{
+		if (this == obj) 
 			return true;
-		}
-		if (!(obj instanceof Physician)) {
+		
+		if (!(obj instanceof Physician)) 
 			return false;
-		}
+		
 		Physician other = (Physician) obj;
+	
 		return id != other.id;
 	}
 
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		StringBuilder builder = new StringBuilder();
+		
 		builder.append("Physician[id = ").append(id);
-		if (lastName != null) {
+		
+		if (lastName != null) 
 			builder.append(", lastName = ").append(lastName);
-		}
-		if (firstName != null) {
+		
+		if (firstName != null) 
 			builder.append(", firstName = ").append(firstName);
-		}
-		if (email != null) {
+		
+		if (email != null) 
 			builder.append(", email = ").append(email);
-		}
-		if (phoneNumber != null) {
+		
+		if (phoneNumber != null) 
 			builder.append(", phoneNumber = ").append(phoneNumber);
-		}
-		//TODO Append the specialty field
-		if (created != null) {
+		
+		if (specialty != null) 
+			builder.append(", specialty = ").append(specialty);
+
+		if (created != null) 
 			builder.append(", created = ").append(created);
-		}
+		
 		return builder.append("]").toString();
 	}
 	
-}
+}//END of Class Physician

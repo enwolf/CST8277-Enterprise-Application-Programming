c786029bfcddacdfe44a55401d957dbb8f49a95b
enwolf <35503014+enwolf@users.noreply.github.com>
2024-10-03 22:02:07 -0400

    Updated Java Class File: ListDataDaoImpl.java
    Updated @SessionScoped to @RequestScoped
    this fixed the null prepared statement issue.
    
    Implemented required methods.
    
    It now correctly populates the drop down menu in the add new user page.

--- Assignment2-DataBank-JSF-JDBC-Physician/src/main/java/databank/dao/ListDataDaoImpl.java
+++ Assignment2-DataBank-JSF-JDBC-Physician/src/main/java/databank/dao/ListDataDaoImpl.java
@@ -15,11 +15,16 @@ import java.util.List;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.context.ExternalContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.servlet.ServletContext;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 @SuppressWarnings("unused")
@@ -29,17 +34,17 @@ import javax.sql.DataSource;
 //TODO Annotate this class so that it becomes a managed bean
 //TODO Provide the proper scope for this managed bean
 @Named
-@SessionScoped
+@RequestScoped
 public class ListDataDaoImpl implements ListDataDao, Serializable {
 	/** Explicitly set serialVersionUID */
 	private static final long serialVersionUID = 1L;
 
 	//TODO Set the value of this string constant properly.  This is the JNDI name
 	//     for the data source.
-	private static final String DATABANK_DS_JNDI = null;
+	private static final String DATABANK_DS_JNDI = "java:app/jdbc/databank";;
 	//TODO Set the value of this string constant properly.  This is the SQL
 	//     statement to retrieve the list of specialties from the database.
-	private static final String READ_ALL_SPECIALTIES = null;
+	private static final String READ_ALL_SPECIALTIES = "SELECT name FROM specialty";
 
 	@Inject
 	protected ExternalContext externalContext;
@@ -50,41 +55,64 @@ public class ListDataDaoImpl implements ListDataDao, Serializable {
 
 	//TODO Use the proper annotation here so that the correct data source object
 	//     will be injected
+	@Resource(name = "java:app/jdbc/databank")
 	protected DataSource databankDS;
-
-	protected Connection conn;
-	protected PreparedStatement readAllSpecialtiesPstmt;
+	protected Connection connectionToDatabase;
+	protected PreparedStatement readAllSpecialtiesPreparedStatement;
 
 	@PostConstruct
 	protected void buildConnectionAndStatements() {
-		try {
-			logMsg("building connection and stmts");
-			conn = databankDS.getConnection();
-			//TODO Initialize PreparedStatement here
-		} catch (Exception e) {
-			logMsg("something went wrong getting connection from database:  " + e.getLocalizedMessage());
-		}
+	    try {
+	        logMsg("Attempting to get connection from data source.");
+
+	        // Establish the connection
+	        connectionToDatabase = databankDS.getConnection();
+	        logMsg("Connection established successfully.");
+
+	        // Initialize the PreparedStatement
+	        readAllSpecialtiesPreparedStatement = connectionToDatabase.prepareStatement(READ_ALL_SPECIALTIES);
+	        logMsg("PreparedStatement initialized successfully.");
+
+	    } catch (Exception e) {
+	        logMsg("Something went wrong getting connection from database: " + e.getLocalizedMessage());
+	        e.printStackTrace();  // Ensure stack trace shows the root cause
+	    }
 	}
+	
+	
 
 	@PreDestroy
-	protected void closeConnectionAndStatements() {
-		try {
+	protected void closeConnectionAndStatements() 
+	{
+		try 
+		{
 			logMsg("closing stmts and connection");
-			//TODO Close PreparedStatement here
-			conn.close();
-		} catch (Exception e) {
+			
+			if (readAllSpecialtiesPreparedStatement != null) readAllSpecialtiesPreparedStatement.close();
+			connectionToDatabase.close();
+		} 
+		catch (Exception e) 
+		{
 			logMsg("something went wrong closing stmts or connection:  " + e.getLocalizedMessage());
 		}
 	}
 
 	@Override
 	public List<String> readAllSpecialties() {
-		logMsg("reading all specialties");
-		List<String> specialties = new ArrayList<>();
-		//TODO Complete the retrieval of all specialties here
-		//TODO Be sure to use try-and-catch statement here
-		return specialties;
-
+	    logMsg("reading all specialties");
+	    List<String> specialties = new ArrayList<>();
+	    try (ResultSet resultSet = readAllSpecialtiesPreparedStatement.executeQuery()) 
+	    {
+	        while (resultSet.next()) 
+	        {
+	            specialties.add(resultSet.getString("name"));  // Correct column name
+	        }
+	    } 
+	    catch (SQLException e) 
+	    {
+	        logMsg("Error reading specialties: " + e.getLocalizedMessage());
+	    }
+	    return specialties;
 	}
 
 }
